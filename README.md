12ecbf37a6816e6324b1fa685b31fb452fa19dfd
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-02-28 10:57:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Virtual Pet JPA
+This is a project made by Alex Malcolm while attending We Can Code IT. This iteration of Virtual Pet takes the project full 
+stack.
---
+++ src/main/java/com/alexjamesmalcolm/virtualpetjpa/VirtualPetJpaApplication.java
@@ -0,0 +1,12 @@
+package com.alexjamesmalcolm.virtualpetjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualPetJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualPetJpaApplication.class, args);
+	}
+}
