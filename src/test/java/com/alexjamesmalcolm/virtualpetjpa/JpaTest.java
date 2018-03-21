a677cb1e43f025b8292cc4697e384b2703a5bfea
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-21 01:13:05 -0400

    Added a passing test for posterity

---
+++ src/test/java/com/alexjamesmalcolm/virtualpetjpa/JpaTest.java
@@ -0,0 +1,20 @@
+package com.alexjamesmalcolm.virtualpetjpa;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class JpaTest {
+	
+	@Resource
+	private CrudRepository<Pet, Long> petRepo;
+	
+	@Test
+	public void shouldInitializePetRepository() {}
+}
