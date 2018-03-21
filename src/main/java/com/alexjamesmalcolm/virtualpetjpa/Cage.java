4ceede6d0e0f719b9ad4a98cfc7cdf4151c627fa
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-21 00:07:29 -0400

    Wired up Cage for JPA

--- src/main/java/com/alexjamesmalcolm/virtualpetjpa/Cage.java
+++ src/main/java/com/alexjamesmalcolm/virtualpetjpa/Cage.java
@@ -1,6 +1,18 @@
 package com.alexjamesmalcolm.virtualpetjpa;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Cage extends Box {
+	
+	@Id
+	@GeneratedValue
+	Long id;
+	@OneToOne
+	Pet pet;
 
 	public Cage(int dirtyness) {
 		this.dirtyness = dirtyness;
